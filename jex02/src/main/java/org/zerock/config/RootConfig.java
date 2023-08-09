845351f75d7f51af8ba66bb14aa240781f609640
Class01 <Class01@DESKTOP-CN96AQ4>
2023-08-09 17:37:06 +0900

    08-09

---
+++ jex02/src/main/java/org/zerock/config/RootConfig.java
@@ -0,0 +1,41 @@
+package org.zerock.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@MapperScan(basePackages = {"org.zerock.mapper"})
+public class RootConfig {
+
+   @Bean
+   public DataSource dataSource() {
+      HikariConfig hikariConfig = new HikariConfig();
+//      hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+      hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+//      hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:xe");
+      hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:XE");
+      hikariConfig.setUsername("c##seungjin");
+      hikariConfig.setPassword("seungjin");
+
+      HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+
+      return dataSource;
+   }
+
+   @Bean
+   public SqlSessionFactory sqlSessionFactory() throws Exception {
+      SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+      sqlSessionFactory.setDataSource(dataSource());
+      return (SqlSessionFactory) sqlSessionFactory.getObject();
+   }
+
+}
